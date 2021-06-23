55e79639c2b62d20a46c4d938019fe163eeb6b3d
meggis <magdalena.siekanska@gmail.com>
2021-06-23 23:49:53 +0200

    add chart bar to power Statistic details

--- src/views/MarvelHeroes.vue
+++ src/views/MarvelHeroes.vue
@@ -2,7 +2,7 @@
   <b-container class="my-5">
     <b-row>
       <b-col>
-        <div                         class="vertical-align">
+        <div class="vertical-align">
           <DxDataGrid
             :data-source="heroPersons"
             :show-borders="true"
@@ -14,18 +14,23 @@
               cell-template="cellTemplate"
             />
             <div slot="cellTemplate" slot-scope="{ data }">
-              <img :src="data.value" class="hero-picture"/>
+              <img :src="data.value" class="hero-picture" />
             </div>
-            <DxColumn dataField="heroName" caption="Hero name"/>
+            <DxColumn dataField="heroName" caption="Hero name" />
             <DxColumn dataField="realName" caption="Real name" />
             <DxColumn dataField="publisher" caption="Publisher" />
-            
-            <DxMasterDetail :enabled="true" template="masterDetailTemplate"/>
+
+            <DxMasterDetail :enabled="true" template="masterDetailTemplate" />
 
             <div slot="masterDetailTemplate" slot-scope="{ data }">
-              <div v-for="(value, key) of data.data.powerStats" :key="key">
-                <span>{{key}} {{value}}</span>
-              </div>
+              <DxChart id="chart" :data-source="getChartObject(data.data.powerStats)">
+                <DxSeries
+                  argument-field="powerStatName"
+                  value-field="powerStatValue"
+                  type="bar"
+                  color="#ffaa66"
+                />
+              </DxChart>
             </div>
           </DxDataGrid>
         </div>
@@ -36,21 +41,22 @@
 
 <script>
 import { DxDataGrid, DxColumn, DxMasterDetail } from "devextreme-vue/data-grid";
+import { DxChart, DxSeries } from "devextreme-vue/chart";
 import { mapState } from "vuex";
-
-// const heroPowerStats = {powerStats}
 
 export default {
   name: "MarvelHeroes",
   components: {
     DxDataGrid,
     DxColumn,
-    DxMasterDetail
+    DxMasterDetail,
+    DxChart, 
+    DxSeries
   },
   data() {
     return {
       response: null,
-      // dataSource: {heroPersons}
+      dataSource: [],
     };
   },
   computed: {
@@ -62,7 +68,21 @@ export default {
   methods: {
     heroPersonClickHandler(heroId) {
       this.$router.push(`./herocard/${heroId}`);
-    }
+    },
+    formatLabel(pointInfo) {
+      return `${pointInfo.valueText} (${pointInfo.percentText})`;
+    },
+    getChartObject(powerStats) {
+      const cartData = [];
+
+      for (const [key, value] of Object.entries(powerStats)) {
+        cartData.push({
+          powerStatName: key,
+          powerStatValue: value,
+        });
+      }
+      return cartData;
+    },
   },
 };
 </script>
@@ -73,7 +93,8 @@ export default {
   height: 9vh;
   width: 9vh;
   object-fit: cover;
-  box-shadow: rgba(1, 3, 3, 0.12) 0px 2px 10px 0px, rgba(0, 0, 0, 0.32) 0px 2px 10px 0px;
+  box-shadow: rgba(1, 3, 3, 0.12) 0px 2px 10px 0px,
+    rgba(0, 0, 0, 0.32) 0px 2px 10px 0px;
 }
 
 .dx-datagrid td {
